d4aed288bd3284db2fcbe55aed435c7a94d1a6b7
Yager1230 <570653543@qq.com>
2022-12-14 00:44:56 +0800

    2022.12.14 首页视频引入

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,18 +3,15 @@ import {
   Route,
   Redirect,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Mindmap from './pages/Mind-map';
-import Home from './pages/Home';
+import Mindmap from "./pages/Mind-map";
+import Home from "./pages/Home";
 
 const AppRouter = () => {
   return (
-
-    // @ts-ignore react18取消children
     <Router>
       <Switch>
-        <Route path="/map" component={Mindmap} />
         <Route path="/map" component={Mindmap} />
         <Route path="/" component={Home} />
         <Redirect to="/" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import "../styles/home.scss";
+
 export default function Home() {
   return (
-    <div>this is Home</div>
-  )
+    <video
+      className="home-video"
+      loop={true}
+      autoPlay={true}
+      src="https://azuki-website.s3.us-west-1.amazonaws.com/beanz2.mp4"
+    ></video>
+  );
 }
